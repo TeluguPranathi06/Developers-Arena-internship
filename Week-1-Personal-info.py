be4ebc192caeb745e3ee579627c8e05dc9b80aed
TeluguPranathi06 <telugupranathi06@gmail.com>
2025-12-13 14:32:07 +0530

    initial commit

---
+++ Week-1-Personal-info.py
@@ -0,0 +1,16 @@
+#PERSONAL INFORMATION MANAGER
+
+Name = str(input("enter your name :"))
+Age = int(input("enter your age :"))
+City = str(input("enter your city name :"))
+hobbies = str(input("enter your hobbies :"))
+dish = str(input("enter your fav food :"))
+color = str(input("enter your fav color :"))
+print("Good Name :",Name)
+print("Age :",Age)
+print("City :",City)
+print("Hobbies :",hobbies)
+print("Favourite Food :",dish)
+print("Favourite Color :",color)
+print("THANKS FOR USING THIS PROGRAM")
+
